8b48e57e665e9d1f9303f59faa75e0febdab49c0
pankozaC++ <77279607+pankoza-pl@users.noreply.github.com>
2023-03-04 08:52:59 +0100

    (hopefully) fixed the memleaks in the source code

--- coore32-GDIOnly/coore32/Program.cs
+++ coore32-GDIOnly/coore32/Program.cs
@@ -2574,11 +2574,38 @@ internal static class Program
     [DllImport("gdi32.dll")]
     public unsafe static extern IntPtr CreateBitmap(int nWidth, int nHeight, uint cPlanes, uint cBitsPerPel, IntPtr lpvBits);
 
-    /// <summary>
-    /// [DllImport("kernel32")]
-    /// </summary>
-    //public static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+    [DllImport("kernel32")]
+    public static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
 
+    [Flags]
+    public enum AllocationType
+    {
+        Commit = 0x1000,
+        Reserve = 0x2000,
+        Decommit = 0x4000,
+        Release = 0x8000,
+        Reset = 0x80000,
+        Physical = 0x400000,
+        TopDown = 0x100000,
+        WriteWatch = 0x200000,
+        LargePages = 0x20000000
+    }
+
+    [Flags]
+    public enum MemoryProtection
+    {
+        Execute = 0x10,
+        ExecuteRead = 0x20,
+        ExecuteReadWrite = 0x40,
+        ExecuteWriteCopy = 0x80,
+        NoAccess = 0x01,
+        ReadOnly = 0x02,
+        ReadWrite = 0x04,
+        WriteCopy = 0x08,
+        GuardModifierflag = 0x100,
+        NoCacheModifierflag = 0x200,
+        WriteCombineModifierflag = 0x400
+    }
 
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public unsafe struct RGBQUAD
